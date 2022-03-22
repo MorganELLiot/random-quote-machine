ddeedef5adf88d9600ea910b3ff5f8979f6aaf96
Morgan Elliot <m.33.elliot@gmial.com>
2022-03-22 20:45:18 +0800

    Updated file structure - ready for development

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div>
+            <h1>Header goes here</h1>
+        </div>
+    )
+}
---
+++ src/components/Quote.js
@@ -0,0 +1,7 @@
+export default function Quote() {
+    return (
+        <div>
+            <h1>Quote goes here</h1>
+        </div>
+    )
+}
